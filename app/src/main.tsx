5cb402c8c9eaadd1bae3c410c0e4f346f3e57e86
wyrmspire <wyrmspire@hotmail.com>
2026-09-21 18:40:25 -0500

    Replace hosted file-kit dependency

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { FileRouter } from '@instinct/files';
+import { FileRouter } from './components/file-kit';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(<FileRouter><App /></FileRouter>);
